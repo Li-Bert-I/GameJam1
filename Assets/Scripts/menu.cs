200c09d045fa32358fa142d6001cb302600a770d
Lina <linalinochka@gmail.com>
2021-02-28 23:27:44 +0300

    All menu

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene("main");
+    }
+
+    public void ShowCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void ShowTutorial() {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
